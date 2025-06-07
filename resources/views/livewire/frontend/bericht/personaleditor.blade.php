349e6134122cb34e8baefbf8d7fecb577cdee31a
dozzemer <soeren@ks-wirth.de>
2025-06-07 14:10:10 +0200

    Update personaleditor.blade.php

--- resources/views/livewire/frontend/bericht/personaleditor.blade.php
+++ resources/views/livewire/frontend/bericht/personaleditor.blade.php
@@ -1,14 +1,16 @@
-<div>
+<div style="max-height: 400px; overflow: auto;">
     @include("layout.error_success")
     <table class="cis-table">
-        <thead>
-            <th>Name</th>
-            <th>
-                Funktion
-            </th>
-            @foreach($einsatzmittel as $em)
-                <th>{{ $em->name }} ( @if(!$em->mannschaft) 0 @else {{ $em->mannschaft }} @endif )</th>
-            @endforeach
+        <thead class="sticky top-0 bg-white z-10">
+            <tr>
+                <th>Name</th>
+                <th>Funktion</th>
+                @foreach($einsatzmittel as $em)
+                    <th>
+                        {{ $em->name }} ({{ $em->fuehrung_count ?? 0 }} / {{ $em->gesamt_count ?? 0 }})
+                    </th>
+                @endforeach
+            </tr>
         </thead>
         <tbody>
         @foreach($users as $user)
